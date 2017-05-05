8519bc32975dfe25e917bce64f567a1042766a8f
rrutherfordd <rrutherfordd@hotmail.com>
2017-05-04 17:39:01 -0700

    Assignment 2 update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-//alert("Assignment 2");
+/*global $ */
+
+$(document).ready(function () {
+  //jQuery loaded?
+  'use strict';
+  //alert("Assignment 2");
+});
 
 var countOfSums = 0, sumOfNumbers = 0, averageOfNumbers = 0, newNumberOrString, fElement, countOfStrings = 0, concatenatedString = "";
 
@@ -7,26 +13,26 @@ function resetNumbers() {
   countOfSums = 0;
   sumOfNumbers = 0;
   averageOfNumbers = 0;
-  
+
   fElement = document.getElementById("countNumber");
   //window.console.log("countNumberEl: " + fElement);
   fElement.innerHTML = "  Count: " + countOfSums;
-  
+
   fElement = document.getElementById("sumNumber");
   //window.console.log("sumNumberEl: " + fElement);
   fElement.innerHTML = "    Sum: " + sumOfNumbers;
-  
+
   fElement = document.getElementById("averageNumber");
   //window.console.log("averageNumberEl: " + fElement);
   fElement.innerHTML = "Average: " + averageOfNumbers;
-  
+
   event.preventDefault();
 }
 
 function calcNumbers() {
   'use strict';
   //alert("Got to calcNumbers" + "typeof newNumberOrString initially: " + typeof(newNumberOrString));
-  
+
   //alert("newNumberOrString: " + newNumberOrString + " typeOf: " + typeof(newNumberOrString));
   countOfSums += 1;
   sumOfNumbers += parseFloat(newNumberOrString);
@@ -35,27 +41,27 @@ function calcNumbers() {
   } else {
     averageOfNumbers = sumOfNumbers / countOfSums;
   }
-  
+
   fElement = document.getElementById("countNumber");
   fElement.innerHTML = "  Count: " + countOfSums;
-  
+
   fElement = document.getElementById("sumNumber");
   fElement.innerHTML = "    Sum: " + sumOfNumbers;
-  
+
   fElement = document.getElementById("averageNumber");
   fElement.innerHTML = "Average: " + averageOfNumbers;
-  
+
   window.console.log("countOfSums: " + countOfSums);
   window.console.log("sumOfNumbers: " + sumOfNumbers);
   window.console.log("averageOfNumbers: " + averageOfNumbers);
-  
+
   event.preventDefault();
 }
 
 function countStrings() {
   'use strict';
   concatenatedString += " " + newNumberOrString;
-  
+
   fElement = document.getElementById("concatenatedString");
   fElement.innerHTML = concatenatedString;
   countOfStrings += 1;
@@ -69,7 +75,7 @@ function newNumberOrStringCheck() {
   'use strict';
   //window.alert("got to newNumberOrStringCheck ");
   newNumberOrString = $('#numberOrString').val();
-  
+
   //newNumberOrString = document.getElementById("numberOrString").value;
   window.console.log("newNumberOrString: " + newNumberOrString);
   window.console.log("typeof newNumberOrString: " + typeof newNumberOrString);
@@ -81,13 +87,12 @@ function newNumberOrStringCheck() {
   } else {
     window.alert("Input must be a number or string!");
   }
-  
+
   event.preventDefault();
 }
 
 /*
 $("#acceptNumbersAndStrings").submit(function (event) {
-  'use strict';
   newNumberOrStringCheck();
   //window.alert("Handler for .submit() called.");
   event.preventDefault();
